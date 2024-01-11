e205e1029c9a55851a090c40f849769390fba0ef
santaclous112 <santaclous112@gmail.com>
2024-01-11 03:31:48 -0800

    signup/signin commit

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-const mongoose = require('mongoose');
+app.use(bodyParser.json());
+
+var cors = require("cors");
+app.use(cors()); // Use this after the variable declaration
+
+const mongoose = require("mongoose");
 // const router = express.Router();
-const users = require('./src/api/users')
+const users = require("./src/api/users");
 
-mongoose.connect('mongodb://127.0.0.1:27017/my_database')
-  .then(() => console.log('MongoDB Connected!!!!!'));
+mongoose
+  .connect("mongodb://127.0.0.1:27017/my_database")
+  .then(() => console.log("MongoDB Connected!!!!!"));
 
-app.get('/test', (req, res) => {
-    res.send('Hello, World!');
+app.get("/test", (req, res) => {
+  res.send("Hello, World!");
 });
 
-app.use('/api/users', users);
+app.use("/api/users", users);
 
 app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+  console.log("Server is running on port 5000");
 });
